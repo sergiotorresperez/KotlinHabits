8836001ff7f3288a03e2b0a6c23b2c82c7d4a939
Sergio Torres <sergiotorresperez@gmail.com>
2018-03-04 16:53:11 +0000

    Defined db

---
+++ app/src/main/java/com/garrapeta/db/Contracts.kt
@@ -0,0 +1,17 @@
+package com.garrapeta.db
+
+import android.provider.BaseColumns
+
+
+val DATABASE_NAME= "habits.db"
+val DATABASE_VERSION = 10
+
+
+object HabitEntry : BaseColumns {
+    val TABLE_NAME = "habit"
+
+    val COL_ID = "_id"
+    val COL_TITLE = "title"
+    val COL_DESCRIPTION = "description"
+    val COL_IMAGE = "image"
+}
---
+++ app/src/main/java/com/garrapeta/db/HabitTrainerDb.kt.kt
@@ -0,0 +1,28 @@
+package com.garrapeta.db
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class HabitTrainerSQLiteOpenHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    private val SQL_CREATE_ENTRIES = "CREATE TABLE ${HabitEntry.TABLE_NAME} (" +
+            "${HabitEntry.COL_ID} INTEGER PRIMARY KEY, " +
+            "${HabitEntry.COL_TITLE} TEXT, " +
+            "${HabitEntry.COL_DESCRIPTION} TEXT, " +
+            "${HabitEntry.COL_IMAGE} BlOB" +
+            ")"
+
+    private val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS ${HabitEntry.TABLE_NAME}"
+
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(SQL_CREATE_ENTRIES)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL(SQL_DELETE_ENTRIES)
+        onCreate(db)
+    }
+
+}
