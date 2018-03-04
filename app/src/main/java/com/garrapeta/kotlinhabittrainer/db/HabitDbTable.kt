e1db09cd859dc309533b0861d6f44b4f8d166fbb
Sergio Torres <sergiotorresperez@gmail.com>
2018-03-04 17:33:26 +0000

    Impemeted insertion with transaction

--- app/src/main/java/com/garrapeta/kotlinhabittrainer/db/HabitDbTable.kt
+++ app/src/main/java/com/garrapeta/kotlinhabittrainer/db/HabitDbTable.kt
@@ -11,16 +11,27 @@ class HabitDbTable(context : Context) {
 
     private val dbHelper = HabitTrainerSQLiteOpenHelper(context)
 
-    fun store(habit : Habit) {
+    fun store(habit : Habit) : Long? {
 
         val db: SQLiteDatabase = dbHelper.writableDatabase
 
         db.use {
             with(ContentValues()) {
+
                 put(HabitEntry.COL_TITLE, habit.title)
                 put(HabitEntry.COL_DESCRIPTION, habit.description)
                 put(HabitEntry.COL_IMAGE, toByteArray(habit.imageBitmap))
-                db.insert(HabitEntry.TABLE_NAME, null, this)
+
+                return try {
+                    db.beginTransaction()
+                    val returnValue = db.insert(HabitEntry.TABLE_NAME, null, this)
+                    db.setTransactionSuccessful()
+                    returnValue
+                } catch (e : Exception) {
+                    null
+                } finally {
+                    db.endTransaction()
+                }
             }
         }
 
